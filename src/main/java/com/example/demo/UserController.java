a8fc6ccc6845ef8718f7f8cf9a8ede143e1de002
DurgaPrasadK18 <durgaprasadkdp18@gmail.com>
2022-01-21 09:03:59 +0530

    added mysql database connection

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -4,13 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 import com.example.demo.entity.User;
+import com.example.demo.service.IUserService;
 
 @RestController
 public class UserController {
+	@Autowired
+	IUserService userService;
     @GetMapping("/user")
 	String getUser() {
     System.out.println("testing");
@@ -19,10 +23,11 @@ public class UserController {
     @PostMapping("/user")
     void createUser(@RequestBody User user) {
     	System.out.println(user.getName());
+    	userService.saveUser(user);
     
     }
     @DeleteMapping("/user")
     void deleteUser() {
-    	//added some method
+    	
     	}  
 }
